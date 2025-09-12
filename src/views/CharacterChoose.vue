2be8d4cf950000d86fa47ac6a92490fa271af94d
JustFr4nck <francyperry07@gmail.com>
2025-09-12 14:04:32 +0200

    starting script gameù

--- src/views/CharacterChoose.vue
+++ src/views/CharacterChoose.vue
@@ -7,20 +7,20 @@
     <div class="flex flex-col basis-1/3 items-center justify-center">
       <label class="bungee-spice-regular" for="">Insert names:</label>
       <input
-        class="border-solid border-black border-1 rounded-lg mb-3 font-bold"
+        class="border-solid border-white border-1 rounded-lg mb-3 font-bold text-white"
         placeholder="player1"
         type="text"
         name=""
         v-model="nik1"
       />
       <input
-        class="border-solid border-black border-1 rounded-lg font-bold mb-5"
+        class="border-solid border-white border-1 rounded-lg font-bold mb-5 text-white"
         placeholder="player2"
         type="text"
         name=""
         v-model="nik2"
       />
-      <button @click="play" class="readyBtn p-1 pr-10 pl-10">Play</button>
+      <button @click="play" class="readyBtn p-1 pr-10 pl-10 text-white">Play</button>
     </div>
     <div class="flex flex-col basis-1/3 items-center justify-center">
       <h2 class="text-3xl bungee-spice-regular">Player2</h2>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,34 +1,34 @@
 <template>
   <div class="h-screen flex flex-row justify-center">
-    <div class="basis-1/3 text-center">
+    <div class="basis-1/3 text-center mt-10 text-3xl">
       <h3 class="bungee-spice-regular">{{ store.player1 }}</h3>
       <img src="./public/images/cat.png" alt="" />
     </div>
 
-    <div class="basis-1/3 text-center">
-      <div>
+    <div class="basis-1/3 flex flex-col items-center">
+      <div class="flex flex-row items-center justify-center border-orange-400 border-solid border-2 mt-30">
         <div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div @click="addImg"  class="internalGrid"> <img v-if="img"  :src="img" alt=""></div>
+          <div class="internalGrid"></div>
+          <div class="internalGrid"></div>
         </div>
         <div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div class="internalGrid"></div>
+          <div class="internalGrid"></div>
+          <div class="internalGrid"></div>
         </div>
         <div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div class="internalGrid"></div>
+          <div class="internalGrid"></div>
+          <div class="internalGrid"></div>
         </div>
       </div>
       <div>
-        <button>Refresh</button>
+        <button class="readyBtn text-white p-1 pr-10 pl-10 mt-4">Refresh</button>
       </div>
     </div>
 
-    <div class="basis-1/3 text-center">
+    <div class="basis-1/3 text-center mt-10 text-3xl">
       <h3 class="bungee-spice-regular">{{ store.player2 }}</h3>
       <img src="./public/images/dog.png" alt="" />
     </div>
@@ -42,6 +42,32 @@
 <script setup>
 import { playersData } from '../../data/store'
 import { resetWindow, putImg, checkIsVoid, nikWin, colorTurn, checkAlgo, whoWin, winFlag} from '../lib/script.js';
+import { ref } from 'vue';
+
+const img = ref('');
+
+const p1Img = "../../public/images/gokuIcon.png";
+const p2Img = "../../public/images/vegetaIcon.png";
+
+let matrix = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
+function addImg(){
+
+    if(putImg() === "tP1"){
+
+       img.value = p1Img;
+       
+    }
+    else{
+        img.value = p2Img
+    }
+
+}
+
 
 const store = playersData()
 </script>
